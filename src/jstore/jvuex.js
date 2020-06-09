5f52a6fd611bc2b0c3fc71a0d79964b42c269951
zhangjingsong <zhangjingsong>
2020-06-10 07:38:35 +0800

    另一种getters的实现方式

--- src/jstore/jvuex.js
+++ src/jstore/jvuex.js
@@ -3,7 +3,7 @@
  * @Author: zhangjingsong 
  * @Date: 2020-06-03 16:02:00 
  * @Last Modified by: zhangjingsong
- * @Last Modified time: 2020-06-03 17:24:16
+ * @Last Modified time: 2020-06-10 07:37:17
  */
 // 保存Vue构造函数
 let Vue
@@ -13,19 +13,43 @@ class Store {
     constructor(options) {
         const store = this
         // 借鸡生蛋,利用data创建响应式state
+        // this._vm = new Vue({
+        //     data: {
+        //         $$state: options.state
+        //     },
+        //     computed: {
+        //         getters() {
+        //             const {...other} = options.getters
+        //             for(let funcName in other) {
+        //                 other[funcName] = other[funcName](store.state)
+        //             }
+        //             return other
+        //         }
+        //     }
+        // })
+        const computed = {}
+        this.getters = {}
+        this._getters = options.getters
+
+        Object.keys(this._getters).map(keys => {
+            const fn = store._getters[keys]
+            
+            computed[keys] = () => {
+                return fn(store.state)
+            }
+
+            Object.defineProperty(store.getters, keys, {
+                get() {
+                    return store._vm[keys]
+                }
+            })
+        })
+
         this._vm = new Vue({
             data: {
-                $$state: options.state
+                $$state:options.state
             },
-            computed: {
-                getters() {
-                    const {...other} = options.getters
-                    for(let funcName in other) {
-                        other[funcName] = other[funcName](store.state)
-                    }
-                    return other
-                }
-            }
+            computed
         })
 
         // 保存mutations、actions
@@ -44,13 +68,13 @@ class Store {
         console.error('please use replaceState to reset state')
     }
 
-    get getters() {
-        return this._vm.getters
-    }
+    // get getters() {
+    //     return this._vm.getters
+    // }
 
-    set getters(v) {
-        console.error('打咩，打咩德斯')
-    }
+    // set getters(v) {
+    //     console.error('打咩，打咩德斯')
+    // }
 
     commit(type, payload) {
         const entry = this._mutations[type]
